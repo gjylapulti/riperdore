484afabee5120ce1e71ef3ebfc18d79980348c42
gjylapulti <gp48517@ubt-uni.net>
2024-01-10 20:19:09 +0100

    Fronted of product card done

--- frontend/src/components/Route/ProductCard/ProductCard.jsx
+++ frontend/src/components/Route/ProductCard/ProductCard.jsx
@@ -9,6 +9,7 @@ import {
   AiOutlineEye,
   AiOutlineShoppingCart,
 } from "react-icons/ai";
+import ProductDetailsCard from "../ProductDetailsCard/ProductDetailsCard";
 
 const ProductCard = ({ data }) => {
   const [click, setClick] = useState(false);
@@ -92,6 +93,7 @@ const ProductCard = ({ data }) => {
             color="#444"
             title="Add to cart"
           />
+          {open ? <ProductDetailsCard setOpen={setOpen} data={data} /> : null}
         </div>
       </div>
     </>
