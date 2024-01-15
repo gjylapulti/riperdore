0f60f1201a64a6948e346b6f7d1a06976281cd42
gjylapulti <gp48517@ubt-uni.net>
2024-01-15 18:25:17 +0100

    Header for mobile - frontend

--- frontend/src/components/Layout/Navbar.jsx
+++ frontend/src/components/Layout/Navbar.jsx
@@ -1,19 +1,21 @@
 import React from "react";
-import { navItems } from "../../static/data";
 import { Link } from "react-router-dom";
-import styles from "./../../styles/styles";
+import { navItems } from "../../static/data";
+import styles from "../../styles/styles";
 
 const Navbar = ({ active }) => {
   return (
-    <div className={`${styles.noramlFlex}`}>
+    <div className={`block 800px:${styles.noramlFlex}`}>
       {navItems &&
         navItems.map((i, index) => (
           <div className="flex">
             <Link
               to={i.url}
               className={`${
-                active === index + 1 ? "text-[black]" : "text-[#ffff]"
-              } font-[500] px-6 cursor-pointer`}
+                active === index + 1
+                  ? "text-[#fc3dd9]"
+                  : "text-black 800px:text-[#fff]"
+              } pb-[30px] 800px:pb-0 font-[500] px-6 cursor-pointer}`}
             >
               {i.title}
             </Link>
